75847ce36ddb055c069d858c21e9850ad57759ca
ArnaudFB <favrebarnaud@gmail.com>
2024-03-11 20:50:16 +0100

    First commit

---
+++ reduction_dimension.R
@@ -0,0 +1,16 @@
+# Library
+
+library(dplyr)
+
+# Clear environnement
+
+rm(list = ls())
+
+# Import des données
+
+load(file = "nappe_data_config_0.RData")
+load(file = "nappe_data_config_1.RData")
+load(file = "nappe_data_config_2.RData")
+load(file = "nappe_data_config_3.RData")
+load(file = "nappe_data_config_4.RData")
+load(file = "nappe_data_config_5.RData")
